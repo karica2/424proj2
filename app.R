13c7f8a492c405b8751e12e82f22dfd4a835bd44
Kenan <karica2@uic.edu>
2022-03-09 09:13:31 -0600

    Revert "Add files via upload"
    This reverts commit e094dd9f2a95607b078409b3b71793dad11d93c2.

--- app.R
+++ app.R
@@ -12,9 +12,7 @@ library(scales)
 library(leaflet)
 # library(tidyverse)
 
-#setwd("~/class/424/424proj2")
-setwd("C:/Users/guagu/OneDrive/Desktop/424proj2-main")
-
+setwd("~/class/424/424proj2")
 latlong <- read.table(file = "latlong.tsv", sep = "\t", header = TRUE, quote = "" , fill=TRUE, row.names = NULL)
 
 # for some reason these lines of code only work when the current dir is datafiles. 
@@ -26,9 +24,9 @@ setwd("datafiles")
 # I checked with the professor if I could use this solution in a piazza post and got the green light. 
 files = list.files(".", ".csv", full.names = TRUE)
 # READ: for testing purposes, you only need to run this code once, as it takes like 13-15 seconds. once you have the data in your env, you can comment it out. 
-summed <- do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+# summed <- do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
 names(latlong)[names(latlong)=="MAP_ID"] <- "station_id"
-print(summed)
+
 latlong_unique <-  latlong[!duplicated(latlong$station_id), ]
 latlong_unique <- latlong_unique[order(latlong_unique$STATION_NAME), ]
 
@@ -68,75 +66,6 @@ pinkLine <- summed[summed$station_id %in% latlong[latlong$Pnk == "true", ]$stati
 
 allStops <- data.frame(latlong_unique$station_id, latlong_unique$STATION_NAME)
 
-#### kevin section
-
-# unused for now
-# mapIcons <- iconList(
-#   red = makeIcon("redMarker.png"),
-#   blue = makeIcon("blueMarker.png"),
-#   brown = makeIcon("brownMarker.png"),
-#   green = makeIcon("greenMarker.png"),
-#   orange = makeIcon("orangeMarker.png"),
-#   purple = makeIcon("purpleMarker.png"),
-#   purple_exp = makeIcon("purpleMarker.png"),
-#   pink = makeIcon("pinkMarker.png"),
-#   yellow = makeIcon("yellowMarker.png"),
-#   grey = makeIcon("greyMarker.png")
-# )
-
-#View(summed)
-# map data is used to create the vectors that are given to the leaflet for markers
-mapData <- data.frame(summed$loc, summed$stationname,summed$station_id)
-mapData <- unique.data.frame(mapData)
-colnames(mapData) <- c("loc","stationname","station_id")
-#View(mapData)
-
-latVector <- vector(mode="numeric", length=0)
-longVector <- vector(mode="numeric", length=0)
-nameVector <- vector(mode = "character", length=0)
-idVector <- vector(mode = "character", length=0)
-colorVector <- vector(mode = "character", length=0)
-
-
-latString = ""
-longString = ""
-tempString = ""
-nameString = ""
-colorString = ""
-idString = ""
-index = 0
-
-# for loop just grabs each value and adds it to the corresponding vector
-# the lat long strings are hard sub stringed to those chars so if one is in a
-# different format it probably will be discarded or plotted way off
-for(row in 1:nrow(mapData)) {
-
-  tempString <- mapData[row,"loc"]
-  latString <-substring(tempString,2,10)
-  longString <- substring(tempString,13,22)
-
-  latVector[row]  <- as.numeric(latString)
-  longVector[row] <- as.numeric(longString)
-
-  tempNameString <- mapData[row,"stationname"]
-  nameString <- tempNameString
-  nameVector[row] <- nameString
-  
-  tempIdString <- mapData[row,"station_id"]
-  idString <- tempIdString
-  idVector[row] <- idString
-
-  # tempColorString <- mapData[row,"color"]
-  # colorString <- tempColorString
-  # colorVector[row] <- colorString
-  # #print(colorVector[row])
-
-  index <- index + 1
-
-}
-
-##################
-
 ui <- fluidPage(
   "Hello, world!",
   fluidRow(
@@ -154,8 +83,7 @@ ui <- fluidPage(
             fluidRow(textOutput("selectedStopTitle"))
            ),
     
-    column(width = 6, plotOutput("allStops")),
-    column(width = 3, leafletOutput("map",height="100vh"))
+    column(width = 9, plotOutput("allStops"))
   )
 )
 server <- function(input, output, session) {
@@ -173,17 +101,13 @@ server <- function(input, output, session) {
     updateSelectizeInput(session = session, inputId = "currentStop", choices = getLineValues(input$currentLine), selected = getLineValues(input$currentLine)[1])
   })
   
-  allStopsReactive <- reactive({ subset(summed, summed$date_ymd == input$currentDate) })
+  allStopsReactive <- reactive({ subset(summed, date_ymd == input$currentDate) })
   selectedStopReactive <- reactive({ subset(latlong_unique, STATION_NAME == input$currentStop ) })
   selectedStopDataReactive <- reactive({ subset(summed, STATION_NAME == input$currentStop )})
   
   output$allStops <- renderPlot({
     
     allData <- allStopsReactive()
-    print("current_stop")
-
-    print(allData)
-    
     ggplot(data=allData, aes(x=stationname, y=rides)) + geom_bar(stat = "identity", fill="#098CF9") + scale_y_continuous("Rides", labels = scales::comma) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
   
     })
@@ -191,22 +115,10 @@ server <- function(input, output, session) {
   output$selectedStopTitle <- renderText(
     {
       current_stop <- selectedStopReactive()
+      print(current_stop)
       paste("Ridership for ", input$currentStop)
     }
   )
-  
-  # uses default map, to change add the line
-  # addProviderTiles("Esri.WorldImagery") %>%
-  # or 
-  # addProviderTiles("Esri.WorldGrayCanvas") %>%
-  # we can probably just make a reactive variable with those strings to change it
-  # quickly
-  output$map <- renderLeaflet({
-    leaflet() %>%
-      addTiles() %>%  
-      setView(lng =-87.658323, lat = 41.879036, zoom = 12) %>%
-      addMarkers(lat = latVector[1:144], lng = longVector[1:144], label = nameVector[1:144])
-  })
   
 }
 shinyApp(ui, server)
